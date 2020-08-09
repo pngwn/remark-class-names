05b464ca740e38be293a384dbdb4be012ba7fb1f
pngwn <hello@pngwn.io>
2020-08-09 11:05:17 +0100

    deep prop helper

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ function remark_class_names(opts) {
 	return add_class_names;
 }
 
+function ensure_deep_props(props, leaf, obj) {
+	let _obj = obj;
+
+	for (let i = 0; i < props.length - 1; i++) {
+		if (!_obj[props[i]]) _obj[props[i]] = {};
+		_obj = _obj[props[i]]
+	}
+
+	_obj[props[props.length - 1]] = leaf;
+
+	return obj;
+}
+
 module.exports = remark_class_names;
