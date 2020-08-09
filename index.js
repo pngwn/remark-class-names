63c49b6e6782ca93fc5758ee09dc49bc6784f410
pngwn <hello@pngwn.io>
2020-08-09 11:15:02 +0100

    make the thing

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-function remark_class_names(opts) {
+const select = require('unist-util-select');
+
+function remark_class_names({ classMap = {} }) {
 	function add_class_names(tree) {
-		console.log(tree)
+		for (const key in classMap) {
+			const nodes = select(key, tree);
+
+			for (let i = 0; i < nodes.length; i++) {
+				nodes[i] = ensure_deep_props(nodes[i]);
+				nodes[i].data.hProperties.className.push(classMap[key]);
+			}
+		}
 	}
 
 	return add_class_names;
@@ -9,12 +18,13 @@ function remark_class_names(opts) {
 function ensure_deep_props(props, leaf, obj) {
 	let _obj = obj;
 
-	for (let i = 0; i < props.length - 1; i++) {
-		if (!_obj[props[i]]) _obj[props[i]] = {};
+	for (let i = 0; i < props.length; i++) {
+		if (!_obj[props[i]]) {
+			if (i === props.length - 1) _obj[props[i]] = leaf;
+			else _obj[props[i]] = {};  
+		}
 		_obj = _obj[props[i]]
 	}
-
-	_obj[props[props.length - 1]] = leaf;
 
 	return obj;
 }
